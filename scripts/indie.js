4421965e79723bfa6560ac85bd0c0f170ca62a01
DiZZert <dizzertmaster@gmail.com>
2021-11-20 00:38:38 +0900

    добавлены все иконки и шрифт

--- scripts/indie.js
+++ scripts/indie.js
@@ -1,23 +1,23 @@
 ( function( $ ) {
 
     var genresArray =
-     '[{"name": "долгие","pic": "long", "games": ["Subnautica", "Outer Wilds", "Kingdom Come", "My Summer Car", "Pathologic 2", "Hollow Knight"]},'+
-     '{"name": "глубокие","pic": "deep",  "games": ["Finding Paradise", "Sally Face", "The Forgotten City", "What Remains of Edith Finch", "The Hex", "Brothers - A Tale of Two Sons", "Detention"]},'+
+     '[{"name": "долгие","pic": "dolgie", "games": ["Subnautica", "Outer Wilds", "Kingdom Come", "My Summer Car", "Pathologic 2"]},'+
+     '{"name": "глубокие","pic": "glubokie",  "games": ["Finding Paradise", "Sally Face", "The Forgotten City", "What Remains of Edith Finch", "The Hex"]},'+
      '{"name": "хоррор","pic": "horror"},'+
-     '{"name": "интерактив","pic": "interactive"},'+
-     '{"name": "культовые","pic": "kult"},'+
-     '{"name": "метроидвания","pic": "metro"},'+
-     '{"name": "платформер","pic": "long"},'+
-     '{"name": "паззл","pic": "long"},'+
-     '{"name": "роглайк","pic": "long"},'+
-     '{"name": "шутер","pic": "long"},'+
-     '{"name": "сложные","pic": "long"},'+
-     '{"name": "интеграция","pic": "long"},'+
-     '{"name": "юмор","pic": "long"},'+
-     '{"name": "мемы","pic": "long"},'+
-     '{"name": "АА инди","pic": "long"},'+
-     '{"name": "саундтрек","pic": "long"},'+
-     '{"name": "random","pic": "long"}]';
+     '{"name": "интерактив","pic": "interaktiv"},'+
+     '{"name": "культовые","pic": "kultovie"},'+
+     '{"name": "метроидвания","pic": "metroidv"},'+
+     '{"name": "платформер","pic": "platform"},'+
+     '{"name": "паззл","pic": "puzzle"},'+
+     '{"name": "роглайк","pic": "roguelike"},'+
+     '{"name": "шутер","pic": "shooter"},'+
+     '{"name": "сложные","pic": "slognie"},'+
+     '{"name": "интеграция","pic": "integration"},'+
+     '{"name": "юмор","pic": "jumor"},'+
+     '{"name": "мемы","pic": "meme"},'+
+     '{"name": "АА инди","pic": "aaindi"},'+
+     '{"name": "саундтрек","pic": "soundtrack"},'+
+     '{"name": "random","pic": "random"}]';
 
      var jsonGenres = jQuery.parseJSON(genresArray);
 
@@ -39,7 +39,7 @@
 
        for ( let i = 0; i < jsonGenres.length; i++ ) {
          $( '<div>', {class: 'icon' + i} )
-         .html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `_active.png" width="66" height="76"> </span>` ).appendTo( $genreIconsClass )
+         .html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Aktiv.png" width="66" height="76"> </span>` ).appendTo( $genreIconsClass )
           .mousedown(function(e){
             switch(e.which) {
               case 1:
@@ -53,12 +53,12 @@
                 }
               break;
               case 2:
-                $( '.icon' + i ).html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `_inactive.png" width="66" height="76"> </span>` )
+                $( '.icon' + i ).html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Neaktiv.png" width="66" height="76"> </span>` )
               break;
               case 3:
                 document.oncontextmenu = function() {return false;};
                 console.log("right on " + jsonGenres[i].name);
-                $( '.icon' + i ).html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `_active.png" width="66" height="76"> </span>` )
+                $( '.icon' + i ).html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Aktiv.png" width="66" height="76"> </span>` )
               break;
             }
             return true;
