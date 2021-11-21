fc9b126d43889a63ba7ca01a53f5b5f4e690f868
DiZZert <dizzertmaster@gmail.com>
2021-11-21 12:04:12 +0300

    random category function

--- scripts/indie.js
+++ scripts/indie.js
@@ -1,23 +1,26 @@
 ( function( $ ) {
 
     var genresArray =
-     '[{"name": "долгие","pic": "dolgie", "games": ["Subnautica", "Outer Wilds", "Kingdom Come", "My Summer Car", "Pathologic 2"]},'+
-     '{"name": "глубокие","pic": "glubokie",  "games": ["Finding Paradise", "Sally Face", "The Forgotten City", "What Remains of Edith Finch", "The Hex"]},'+
-     '{"name": "хоррор","pic": "horror"},'+
-     '{"name": "интерактив","pic": "interaktiv"},'+
-     '{"name": "культовые","pic": "kultovie"},'+
-     '{"name": "метроидвания","pic": "metroidv"},'+
-     '{"name": "платформер","pic": "platform"},'+
-     '{"name": "паззл","pic": "puzzle"},'+
-     '{"name": "роглайк","pic": "roguelike"},'+
-     '{"name": "шутер","pic": "shooter"},'+
-     '{"name": "сложные","pic": "slognie"},'+
-     '{"name": "интеграция","pic": "integration"},'+
-     '{"name": "юмор","pic": "jumor"},'+
-     '{"name": "мемы","pic": "meme"},'+
-     '{"name": "АА инди","pic": "aaindi"},'+
-     '{"name": "саундтрек","pic": "soundtrack"},'+
+     '[{"name": "долгие","pic": "dolgie", "games": ["Subnautica", "Desperados III", "Disco Elysium", "Pathologic 2", "Hollow Knight"]},'+
+     '{"name": "глубокие","pic": "glubokie",  "games": ["The Vanishing of Ethan Carter", "What Remains of Edith Finch", "The Red Strings Club", "The Beginner’s Guide", "OneShot"]},'+
+     '{"name": "хоррор","pic": "horror", "games": ["Summer of 58", "Yuppie Psycho", "Among the Sleep", "Detention", "Happy Game"]},'+
+     '{"name": "экшн игры","pic": "random", "games": ["Death’s door", "Elderborn", "Alien Shooter", "Ape Out", "Chernobylite"]},'+ //нет иконок
+     '{"name": "культовые","pic": "kultovie", "games": ["Papers, Please", "Undertale", "The Stanley Parable", "Brothers - A Tale of Two Sons", "Braid"]},'+
+     '{"name": "метроидвания","pic": "metroidv", "games": ["Ori and the Will of the Wisps", "Ender Lilies: Quietus of the Knights", "Fearmonium", "F.I.S.T.: Forged In Shadow Torch", "Valfaris"]},'+
+     '{"name": "платформер","pic": "platform", "games": ["Child of Light", "Little Nightmares 2", "Pumpkin Jack", "Psychonauts", "A Story About My Uncle"]},'+
+     '{"name": "приключения","pic": "random", "games": ["Tails of Iron", "Grimm’s Hollow", "Sally Face", "The Banner Saga", "LISA: The Painfull"]},'+ //нет иконок
+     '{"name": "роглайк","pic": "roguelike", "games": ["Dead Cells", "Inscryption", "Hades", "BPM: Bullets Per Minute", "Ziggurat 2"]},'+
+     '{"name": "шутер","pic": "shooter", "games": ["Bright Memory: Infinite", "SUPERHOT", "Shadow Warrior 2", "Serious Sam: The First Encounter", "Black Mesa"]},'+
+     '{"name": "интерактивные истории","pic": "random", "games": ["Her Story", "Death and Taxes", "Road 96", "Not For Broadcast", "Late Shift"]},'+ //нет иконок
+     '{"name": "Хайден Гемы","pic": "random", "games": ["In Sound Mind", "Inertial Drift", "A Juggler’s Tale", "Bright Lights of Svetlov", "Sludge Life"]},'+ //нет иконок
+     '{"name": "юмор","pic": "jumor", "games": ["Protocol", "Untitled Goose Game", "Pony Island", "Breathedge", "There Is No Game: Wrong Dimension"]},'+
+     '{"name": "мемы","pic": "meme", "games": ["Mercury Man", "Montaro", "Adventures of pepeL", "I Hate this Game", "Helltaker"]},'+
+     '{"name": "АА инди","pic": "aaindi", "games": ["Life is Strange", "KENA: Bridge of Spirits", "Outlast", "Hellblade: Senua’s Sacrifice", "Terminator: Resistance"]},'+
+     '{"name": "саундтрек","pic": "soundtrack", "games": ["Furi", "Ruiner", "Everhood", "Bastion", "198X"]},'+
+     '{"name": "квесты","pic": "random", "games": ["Rusty Lake: Roots", "Grim Fandango", "Superliminal", "Sherlock Holmes: Crimes and Punishments", "Forgotten City"]},'+ //нет иконок
      '{"name": "random","pic": "random"}]';
+
+     var genresActiveArray = [];
 
      var jsonGenres = jQuery.parseJSON(genresArray);
 
@@ -30,8 +33,17 @@
        }
      }
 
+     let lastValue = -1;
+
      function getRandom() {
-       return  Math.floor(Math.random() * (jsonGenres.length - 1 - 0) + 0);
+       randomValue = Math.floor(Math.random() * (genresActiveArray.length - 0) + 0);
+       if (lastValue != randomValue) {
+         console.log(lastValue + " " + randomValue);
+         lastValue = randomValue;
+         return randomValue;
+       } else {
+         return getRandom();
+       }
      }
 
      function doGenreList(data) {
@@ -39,13 +51,14 @@
 
        for ( let i = 0; i < jsonGenres.length; i++ ) {
          $( '<div>', {class: 'icon' + i} )
-         .html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Aktiv.png" width="66" height="76"> </span>` ).appendTo( $genreIconsClass )
+         .html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Neaktiv.png" width="66" height="76"> </span>` ).appendTo( $genreIconsClass )
           .mousedown(function(e){
             switch(e.which) {
               case 1:
                 if (jsonGenres[i].name == "random") {
+                  console.log(genresActiveArray);
                   let randomGenre = getRandom();
-                  $("#genreName").text(jsonGenres[randomGenre].name);
+                  $("#genreName").text(genresActiveArray[randomGenre].name);
                   doGamesList(randomGenre);
                 } else {
                   $("#genreName").text(jsonGenres[i].name);
@@ -53,12 +66,15 @@
                 }
               break;
               case 2:
-                $( '.icon' + i ).html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Neaktiv.png" width="66" height="76"> </span>` )
+                genresActiveArray.push(jsonGenres[i]);
+                console.log(genresActiveArray);
+                $( '.icon' + i ).html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Aktiv.png" width="66" height="76"> </span>` )
               break;
               case 3:
                 document.oncontextmenu = function() {return false;};
-                console.log("right on " + jsonGenres[i].name);
-                $( '.icon' + i ).html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Aktiv.png" width="66" height="76"> </span>` )
+                genresActiveArray.splice($.inArray(genresActiveArray[i], genresActiveArray), 1);
+                console.log(genresActiveArray);
+                $( '.icon' + i ).html( `<span> <img src="assets/icons/` + jsonGenres[i].pic + `Neaktiv.png" width="66" height="76"> </span>` )
               break;
             }
             return true;
